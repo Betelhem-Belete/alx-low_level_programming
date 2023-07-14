6b8d1c2a579564cd31e5e6044e35a2b6aec0e342
Betelhem-Belete <betelhembelete0@gmail.com>
2023-07-14 14:08:48 -0700

    variables, ie else, while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 56; i++)
+	{
+		for (j = 49; j <= 57; j++)
+		{
+			if (j > i)
+			{
+				putchar(i);
+				putchar(j);
+				if (i != 56 || j != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
