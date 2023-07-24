3822c1bf288a6a8b7630bc3ada52d4f6c7511c7b
Betelhem-Belete <betelhembelete0@gmail.com>
2023-07-24 14:47:17 -0700

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * return: length
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s 1= '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
+}
