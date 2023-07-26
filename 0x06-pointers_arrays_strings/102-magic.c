0117fed4d452474e3f88e8bb701363d7297707d4
Betelhem-Belete <betelhembelete0@gmail.com>
2023-07-26 11:39:25 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -17,7 +17,7 @@ int main(void)
  * - only one statment
  * - you are not allowed to code anything else than this line of code
  */
-*(p + 5 = 98;
+*(p + 5) = 98;
 /**
  * ...so that this prints 98\n
  */
