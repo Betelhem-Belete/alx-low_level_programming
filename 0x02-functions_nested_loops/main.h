fae221b976eabfc39f7314910b7532237e19bc8d
Betelhem-Belete <betelhembelete0@gmail.com>
2023-07-26 12:06:24 -0700

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,20 +1,19 @@
-#include "main.h"
-/**
- * main - print _putchar
- * is written by mbah
- * Return: 0
- */
+#ifndef main_h
+#define main_h
 
-int main(void)
-{
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
-}
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
+
