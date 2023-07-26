840b98b2749874104c40beced16d2fc4917bfa7c
Betelhem-Belete <betelhembelete0@gmail.com>
2023-07-26 11:30:50 -0700

    magic

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	int a[s];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+/*
+ * write your line of code here...
+ * Remember:
+ * - you are not allowed to use a
+ * - you are not allowed to modify p
+ * - only one statment
+ * - you are not allowed to code anything else than this line of code
+ */
+*(p + $) = 98;
+/* ...so that this prints 98\n
+*/
+printf("a[2] = %d\n", a[2]);
+return (0);
+}
