33aecaa467344e7e76b2947344f0753534ccf3b7
Betelhem-Belete <betelhembelete0@gmail.com>
2023-07-26 12:32:39 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,14 +12,19 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
+	
 		return (1);
-	} else if (n == 0)
-}
-		_putchar(48);
-		return (0);
-	} else if (n < 0)
+	}else if (n == 0)
 	{
-		_putchar('-');
+	        _putchar(0);
+	 
+		return (0);
 	}
+	else 
+	 {
+		_putchar('-');
+	
 		return (-1);
+	}
 }
+
