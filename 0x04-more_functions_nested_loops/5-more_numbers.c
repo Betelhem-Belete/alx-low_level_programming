e9a2335f7cb9cb0d17939a9d6913ac15d768b070
Betelhem-Belete <betelhembelete0@gmail.com>
2023-07-26 14:15:09 -0700

    num

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *
+ * more_numbers - print more numbers
  */
 
 void more_numbers(void)
@@ -13,7 +13,8 @@ void more_numbers(void)
 		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
-				_putchar (j % 10 * '0');
+				_putchar('1');
+			_putchar (j % 10 + '0');
 		}
 		_putchar('\n');
 	}
